ee871ab86b08152a4f7972ea99ec85b33e72d267
asbebenov <93144015+asbebenov@users.noreply.github.com>
2021-10-26 21:36:05 +0700

    1st

---
+++ resources/views/mypage.blade.php
@@ -0,0 +1,19 @@
+@extends('maket')
+
+@section('content')
+    <table class="table table-bordered">
+        <tr>
+            <th>Page 1</th>
+            <th>Page 2</th>
+        </tr>
+        <tr>
+            <td>
+                <a href="/err">Stranicka 1</a>
+            </td>
+            <td>
+                <a href="/err">Stranicka 2</a>
+            </td>
+
+        </tr>
+    </table>
+@endsection
